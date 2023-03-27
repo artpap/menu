ced53bdafb428381d92e7f6a125314d8151fc973
artpap <100701975+artpap@users.noreply.github.com>
2023-03-27 19:57:45 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import Title from './Title';
+import items from './data';
+import Menu from './Menu';
+import Categories from './Categories';
+import { useState } from 'react';
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='our menu' />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
